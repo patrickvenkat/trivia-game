22ed7811977b38d9826962718a60dbfacde2cffa
Patrick Venkat <patrick.venkat@gmail.com>
2019-07-21 21:39:14 -0400

    Final touches

--- assets/javascript/trivia-game.js
+++ assets/javascript/trivia-game.js
@@ -54,6 +54,34 @@ var triviaQuestions = [{
     answer3: "ans243",
     answer4: "ans244",
     correctAnswer: "ans243"
+    }, {
+    Question: "3rd row 1st question",
+    answer1: "ans311",
+    answer2: "ans312",
+    answer3: "ans313",
+    answer4: "ans314",
+    correctAnswer: "ans314"
+    }, {
+    Question: "3rd row 2nd question",
+    answer1: "ans321",
+    answer2: "ans322",
+    answer3: "ans323",
+    answer4: "ans324",
+    correctAnswer: "ans222"
+    }, {
+    Question: "3rd row 3rd question",
+    answer1: "ans331",
+    answer2: "ans332",
+    answer3: "ans333",
+    answer4: "ans334",
+    correctAnswer: "ans332"
+    }, {
+    Question: "3rd row 4th question",
+    answer1: "ans341",
+    answer2: "ans342",
+    answer3: "ans343",
+    answer4: "ans344",
+    correctAnswer: "ans343"
     }]
 
     var dollarWins = 0;
@@ -61,8 +89,10 @@ var triviaQuestions = [{
     var i; //Question Index
     var timerID;
     var timeRemaining = 0;
+    var countAnswered = 0;
     answersLocked = true;
 
+    // Show the rows hidden by Bootstrap
     showRows();
 
     $("#row1-q1").on("click", function (){
@@ -144,6 +174,46 @@ var triviaQuestions = [{
             timeKeeperFunction();
         }
     });
+    
+    $("#row3-q1").on("click", function (){
+        if (!($("#row3-q1").text() == "Answered") && (timeRemaining === 0)){
+            i = 8;
+            timeRemaining = 10;
+            questionSelection(i, "#row3-q1");
+            questionAmount = 100;
+            timeKeeperFunction();
+        }
+    });
+
+    $("#row3-q2").on("click", function (){
+        if (!($("#row3-q2").text() == "Answered") && (timeRemaining === 0)){
+            i = 9;
+            timeRemaining = 10;
+            questionSelection(i, "#row3-q2");
+            questionAmount = 100;
+            timeKeeperFunction();
+        }
+    });
+
+    $("#row3-q3").on("click", function (){
+        if (!($("#row3-q3").text() == "Answered") && (timeRemaining === 0)){
+            i = 10;
+            timeRemaining = 10;
+            questionSelection(i, "#row3-q3");
+            questionAmount = 100;
+            timeKeeperFunction();
+        }
+    });
+
+    $("#row3-q4").on("click", function (){
+        if (!($("#row3-q4").text() == "Answered") && (timeRemaining === 0)){
+            i = 11;
+            timeRemaining = 10;
+            questionSelection(i, "#row3-q4");
+            questionAmount = 100;
+            timeKeeperFunction();
+        }
+    });
 
     $("#answer-1").on("click", function (){
         if (!(answersLocked)){
@@ -203,8 +273,8 @@ var triviaQuestions = [{
         $("#timekeeper").html("<h1>Time remaining = " + timeRemaining + "</h1>");
     }
 
+    // Code below executes whenever an answer is chosen
     function answerClickFunction(answer, amount){
-
         answersLocked = true;
         if (answer === triviaQuestions[i].correctAnswer){
             dollarWins += amount;
@@ -221,8 +291,15 @@ var triviaQuestions = [{
             timeRemaining = 0;
             showRows();
         }
+        
+        countAnswered ++;
+        if (countAnswered === triviaQuestions.length){
+            $("#timekeeper").html("<h1>Game complete!</h1>");
+        }
+
     }       
     
+    // Code below displays a new question and answer set
     function displayQuestionSet(i){
         $("#question").html(triviaQuestions[i].Question);
         $("#answer-1").html("a)" + triviaQuestions[i].answer1);
@@ -240,6 +317,7 @@ var triviaQuestions = [{
         collapseRows();
     }
     
+    // Code below counts down time for each question
     function timeKeeperFunction() {
         clearInterval(timerID);
         timerID = setInterval(countDown, 1000);
@@ -262,6 +340,7 @@ var triviaQuestions = [{
       clearInterval(timerID);
     }
 
+    // Code below locks the answer once it is chosen
     function lockTheAnswers(){
         if (triviaQuestions[i].answer1 === triviaQuestions[i].correctAnswer){
             $("#answer-1").css("color", "green");
@@ -298,6 +377,7 @@ var triviaQuestions = [{
         answersLocked = true;
     };
 
+    // Code below shows rows hidden
     function showRows(){
         $("#row0").removeClass('d-none');
         $("#row1").removeClass('d-none');
@@ -305,6 +385,7 @@ var triviaQuestions = [{
         $("#row3").removeClass('d-none');
     };
 
+    // Code below hides rows to slide up the question and answer set
     function collapseRows(){
         $("#row0").addClass('d-none');
         $("#row1").addClass('d-none');
